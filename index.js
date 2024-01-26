d005e0f260e0787b20da2f26159eaaedba5c2ad1
Iam-Rohit-debug <82030678+Iam-Rohit-debug@users.noreply.github.com>
2024-01-26 21:53:02 +0530

    update to crudcrud

--- index.js
+++ index.js
@@ -2,61 +2,108 @@ const form = document.querySelector("form");
 function handleFormSubmit(event) {
   event.preventDefault();
   let email = event.target.email.value;
-  let userDetails = {
-    name: event.target.username.value,
-    email: event.target.email.value,
-    tel: event.target.phone.value,
-  };
 
   axios
-    .post(
-      "https://crudcrud.com/api/b8a1fc0a1fb247acbc817e474c2199d1/bookingData",
-      userDetails
+    .get(
+      "https://crudcrud.com/api/427bf718d6264f0d86bee22b57db0e52/bookingData"
     )
     .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      let allUserLength = res.data;
+      console.log(allUserLength);
+      let flag = true;
+      for (let i = 0; i < allUserLength.length; i++) {
+        if (allUserLength[i].email === email) {
+          if (allUserLength[i]._id !== null) {
+            flag = false;
+            let userId = allUserLength[i]._id;
+            console.log(userId);
+            axios.put(
+              `https://crudcrud.com/api/427bf718d6264f0d86bee22b57db0e52/bookingData/${userId}`,
+              {
+                name: event.target.username.value,
+                email: event.target.email.value,
+                tel: event.target.phone.value,
+              }
+            ).then((res)=>{
+                console.log(res)
+                console.log("successfully updated")
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+          }
+        }
+      }
+      if (flag) {
+        console.log("sjn");
+        let userDetails = {
+          name: event.target.username.value,
+          email: event.target.email.value,
+          tel: event.target.phone.value,
+        };
 
-  localStorage.setItem(email, JSON.stringify(userDetails));
-  showUser(userDetails);
+        axios
+          .post(
+            "https://crudcrud.com/api/427bf718d6264f0d86bee22b57db0e52/bookingData",
+            userDetails
+          )
+          .then((res) => {
+            console.log(res.data);
+            showUser(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        localStorage.setItem(email, JSON.stringify(userDetails));
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
+
+function getDataFromUser() {}
 
 function deleteUserDetail(event) {
   const parent = event.target.parentNode;
   const li = parent.getElementsByTagName("li")[1];
   console.log(li.innerText);
-//   const node = JSON.parse(localStorage.getItem(li.innerText));
-//   console.log(node);
-axios.get("https://crudcrud.com/api/b8a1fc0a1fb247acbc817e474c2199d1/bookingData")
-.then((res)=>{
-    // console.log(res.data);
-    const email=res.data;
-    for(let i=0;i<email.length;i++){        
-        if(email[i].email===li.innerText){
-            const id=res.data[i]._id;
-            axios.delete(`https://crudcrud.com/api/b8a1fc0a1fb247acbc817e474c2199d1/bookingData/${id}`)
-            .then((res)=>{
-                console.log(res);
-                console.log("successfully deleted");
+  axios
+    .get(
+      "https://crudcrud.com/api/427bf718d6264f0d86bee22b57db0e52/bookingData"
+    )
+    .then((res) => {
+      // console.log(res.data);
+      const email = res.data;
+      for (let i = 0; i < email.length; i++) {
+        if (email[i].email === li.innerText) {
+          const id = res.data[i]._id;
+          axios
+            .delete(
+              `https://crudcrud.com/api/427bf718d6264f0d86bee22b57db0e52/bookingData/${id}`
+            )
+            .then((res) => {
+              console.log(res);
+              console.log("successfully deleted");
             })
-            .catch((error)=>{
-                console.log(error);
-            })
+            .catch((error) => {
+              console.log(error);
+            });
         }
-    }
-})
-.catch((error)=>{
-    console.log(error);
-})
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-
-//   localStorage.removeItem(li.innerText);
-
+  //   localStorage.removeItem(li.innerText);
 
   parent.remove();
+}
+
+function editUserDetail(event) {
+  const parent = event.target.parentNode;
   if (event.target.classList.contains("edit")) {
     const input = document.querySelectorAll("input");
     for (let i = 0; i < input.length; i++) {
@@ -68,7 +115,7 @@ axios.get("https://crudcrud.com/api/b8a1fc0a1fb247acbc817e474c2199d1/bookingData
 window.addEventListener("DOMContentLoaded", () => {
   axios
     .get(
-      "https://crudcrud.com/api/b8a1fc0a1fb247acbc817e474c2199d1/bookingData"
+      "https://crudcrud.com/api/427bf718d6264f0d86bee22b57db0e52/bookingData"
     )
     .then((res) => {
       const newObj = res.data;
@@ -80,15 +127,15 @@ window.addEventListener("DOMContentLoaded", () => {
       console.log(error);
     });
 
-//   Retrieve data from local Storage
+  //   Retrieve data from local Storage
 
-//   const localStorageObj = localStorage;
-//   const localStorageObjKeys = Object.keys(localStorageObj);
-//   for(let i=0;i<localStorageObjKeys.length;i++){
-//       const eachUser = localStorageObjKeys[i];
-//       const newObj = JSON.parse(localStorage.getItem(eachUser));
-//       showUser(newObj);
-//   }
+  //   const localStorageObj = localStorage;
+  //   const localStorageObjKeys = Object.keys(localStorageObj);
+  //   for(let i=0;i<localStorageObjKeys.length;i++){
+  //       const eachUser = localStorageObjKeys[i];
+  //       const newObj = JSON.parse(localStorage.getItem(eachUser));
+  //       showUser(newObj);
+  //   }
 });
 
 function showUser(newObj) {
@@ -112,7 +159,7 @@ function showUser(newObj) {
 
   editBtn.setAttribute("type", "click");
   editBtn.setAttribute("class", "edit");
-  editBtn.setAttribute("onclick", "deleteUserDetail(event)");
+  editBtn.setAttribute("onclick", "editUserDetail(event)");
   ul.append(btn);
   ul.append(editBtn);
   form.after(ul);
